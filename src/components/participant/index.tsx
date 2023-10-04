8608e2e8cc56bf45cab0af9b08eee2f42726d570
Fabiano-Franca <fabianofranca.ti@gmail.com>
2023-10-04 15:28:56 -0300

    Forma 02: Com Typescript

--- src/components/participant/index.tsx
+++ src/components/participant/index.tsx
@@ -1,7 +1,11 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
-export function Participant({name}){
+type Props = {
+    name: string;
+}
+
+export function Participant({ name }:Props){
     return(
         <View style={styles.container}>
             <Text style={styles.name}>
