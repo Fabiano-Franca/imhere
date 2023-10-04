f027675a328ddb55781439455a3c2fa5d7c0dacf
Fabiano-Franca <fabianofranca.ti@gmail.com>
2023-10-04 15:26:29 -0300

    Forma 02: Passar valores para o componente

--- src/components/participant/index.tsx
+++ src/components/participant/index.tsx
@@ -1,11 +1,11 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
-export function Participant(props){
+export function Participant({name}){
     return(
         <View style={styles.container}>
             <Text style={styles.name}>
-                {props.name}
+                {name}
             </Text>
             <TouchableOpacity style={styles.button}>
                 <Text style={styles.buttonText}>
