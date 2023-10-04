9bca3fd8dacf6f3dfeaeff7cc09b89d8b9467bf8
fabiano-franca <fabianofranca.ti@gmail.com>
2023-10-04 11:45:17 -0300

    Form - Flex Direction

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import { GestureResponderEvent, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+    function handleParticipantAdd(event: GestureResponderEvent): void {
+        console.log("Você clicou no botão de Adicionar!");
+    }
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.eventName}>
+        Nome do evento
+        </Text>
+        <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+        </Text>
+        
+        <View style={styles.form}>
+            <TextInput style={styles.input}
+            placeholder='Nome do participante'
+            placeholderTextColor='#6B6B6B'/>
+
+            <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                <Text style={styles.buttonText}>
+                    +
+                </Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+  );
+}
