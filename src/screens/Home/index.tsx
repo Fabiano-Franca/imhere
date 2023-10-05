b83779d642bc3de34ccc7c79e9fc641bb4868790
Fabiano-Franca <fabianofranca.ti@gmail.com>
2023-10-05 18:31:26 -0300

    Salvando participante no estado - Forma resumida

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -41,7 +41,7 @@ export function Home() {
             <TextInput style={styles.input}
             placeholder='Nome do participante'
             placeholderTextColor='#6B6B6B'
-            onChangeText={text => setParticipantName(text)}
+            onChangeText={setParticipantName}
             value={participantName}
             />
 
