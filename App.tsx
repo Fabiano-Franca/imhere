35d971e4e850eb3b0c64841a3166a659d030f448
fabiano-franca <fabianofranca.ti@gmail.com>
2023-10-05 08:11:29 -0300

    StatusBar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
+import { StatusBar } from "react-native";
 import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
-    <Home />
+    /*
+      Propriedades StatusBar
+      barStyle -> altera a cor dos ícones;
+      backgroundColor -> altera a cor da bara;
+      translucent -> indica de a status bar pode sobrepor 
+      (ficar em cima/ a frente) a tela de app.
+    */
+    <>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
   );
 }
